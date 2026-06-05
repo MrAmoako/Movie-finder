ca2931e9d4e924428419091905c9fe7b26c173ee
MrAmoako <simplecraft1212@gmail.com>
2026-06-05 18:10:18 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const button = document.getElementById('searchBtn')
+const input = document.getElementById('input')
+const error = document.getElementById('error')
+const placeholder = document.getElementById('results')
+
+async function getMovie(query) {
+   try {
+    
+   }
+}
